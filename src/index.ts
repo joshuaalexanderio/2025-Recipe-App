d126addf52953afd2e960d0db0a3484a4c8e97a6
Joshua Alexander <83144672+joshuaalexanderio@users.noreply.github.com>
2025-01-19 15:19:02 -0700

    Remove whitespace in index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,6 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "index.html"))
 );
 
-
-
 app.listen(PORT, () => {
   console.log(`app is listening on http://localhost:${PORT}`);
 });
